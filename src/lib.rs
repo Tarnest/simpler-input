bf99280a7324906b2b9ef597621fe3af7f743ea7
Tarnest <tristanconnolly1@gmail.com>
2021-03-19 21:54:31 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub fn input() -> String {
+	let mut val = String::new();
+	std::io::stdin().read_line(&mut val).expect("Something went wrong with the input.");
+	
+	return val[0..val.len() - 1].to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn input_test() {
+        let a = input();
+        let b = input();
+        
+        assert_eq!(a, "is funny");
+        assert_eq!(b, "test");
+    }
+}
